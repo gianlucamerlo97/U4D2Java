0d6a77d6e8c8be3aba52fda80ac60359cc8ac5fb
gianlucamerlo97 <notoriousbig9700@gmail.com>
2025-06-17 16:34:36 +0200

    last commit

--- src/Es3/Es3.java
+++ src/Es3/Es3.java
@@ -1,4 +1,31 @@
 package Es3;
-
+import java.util.Scanner;
 public class Es3 {
+    public static void main(String[] args) {
+
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void Es3() {
+        String stringa = scanner.next();
+        int i = 0;
+        String testo = "";
+        while (!(stringa.equals(":q"))) {
+
+
+            while (i < stringa.length()) {
+                testo += stringa.charAt(i);
+                if (i < stringa.length() - 1) {
+                    testo += ", ";
+                }
+                i++;
+            }
+            System.out.println(testo);
+            stringa = scanner.next();
+            testo = "";
+
+
+        }
+    }
 }
--- src/Es4/Es4.java
+++ src/Es4/Es4.java
@@ -1,4 +1,9 @@
 package Es4;
 
 public class Es4 {
+    public static void main(String[] args) {
+
+    }
+
+    for(int i=0;i<)
 }
